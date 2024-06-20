f420deededb0464f6007283dce98a95b67d2dbfd
Ümran <58302928+umranugrl@users.noreply.github.com>
2024-06-20 13:29:09 +0300

    add input and output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+//component: angular tarafından bir görünüm ve işlev için küçük parçalardır.
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
+  selector: 'app-root',//template içinde kullanılacak etiket.
+  standalone: true,//angular 17 sonrası için varsayılan hale geldi.
+  //standalone componentler herhangi bir module yapısına bağlı kalmadan var olabilirler.
+  imports: [CommonModule, RouterModule ,NavbarComponent,FooterComponent],//import edilecek modülleri ve angular yapılarını belirtir.
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-app';
+
 }
+
+// Angular 17 öncesinde varsayılan olarak componentler Module yapısında tanımlanıyorlardı.
+// Angular Module Örneği
+// @NgModule({
+//   declarations: [AppComponent], // Component, Directive ve Pipe'ları tanımlar.
+//   imports: [], // Dışarıdan alınan modülleri ekler.
+//   providers: [], // Servisleri ekler.
+//   bootstrap: [AppComponent], // Uygulamanın başlangıç componentini belirler.
+// })
+// export class AppModule {}
---
+++ src/app/routes/auth/login-page/login-page.component.ts
@@ -0,0 +1,18 @@
+import { BasicLayoutComponent } from '../../../shared/components/basic-layout/basic-layout.component';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-login-page',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterModule,
+    BasicLayoutComponent
+  ],
+  templateUrl: './login-page.component.html',
+  styleUrl: './login-page.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LoginPageComponent { }
